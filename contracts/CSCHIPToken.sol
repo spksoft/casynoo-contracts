ff9c2d51b54c2c6a202b1c9428a0b68d9de16bd8
spksoft <spkrsk.37@gmail.com>
2021-12-10 05:57:34 +0700

    fix var

--- contracts/CSCHIPToken.sol
+++ contracts/CSCHIPToken.sol
@@ -1,10 +1,10 @@
-// contracts/GLDToken.sol
+// contracts/CSCHIPToken.sol
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract GLDToken is ERC20 {
+contract CSCHIPToken is ERC20 {
     constructor() ERC20("CasynooCHIP", "CSCHIP") {
         _mint(msg.sender, 1000000000000000000);
     }
