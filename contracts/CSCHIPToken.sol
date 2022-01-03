7d53f4cbbd4bfc6b2392d215c77da71ff97ab7be
spksoft <spkrsk.37@gmail.com>
2022-01-03 16:46:41 +0700

    update init token value

--- contracts/CSCHIPToken.sol
+++ contracts/CSCHIPToken.sol
@@ -6,6 +6,6 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract CSCHIPToken is ERC20 {
     constructor() ERC20("CasynooCHIP", "CSCHIP") {
-        _mint(msg.sender, 20000000 * 10 ** 18);
+        _mint(msg.sender, 21000000 * 10 ** 18);
     }
 }
