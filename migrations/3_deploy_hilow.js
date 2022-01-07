4a6d9621ecd93c7bb9c957f93399f4cd17fc80a1
Sippakorn Raksakiart <spkrsk.37@gmail.com>
2022-01-07 17:13:42 +0700

    Update 3_deploy_hilow.js

--- migrations/3_deploy_hilow.js
+++ migrations/3_deploy_hilow.js
@@ -2,5 +2,5 @@ const HiLow = artifacts.require("HiLow");
 const CSCHIPToken = artifacts.require("CSCHIPToken");
 
 module.exports = function (deployer) {
-  deployer.deploy(HiLow, CSCHIPToken.address, true);
+  deployer.deploy(HiLow, CSCHIPToken.address, false);
 };
